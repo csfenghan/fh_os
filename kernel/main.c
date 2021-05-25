5052468f83db251d1aa0884023f07dd2931b5d47
fenghan <csfenghan@foxmail.com>
2021-05-25 19:47:47 +0800

    测试性修改

--- kernel/main.c
+++ kernel/main.c
@@ -5,13 +5,15 @@
 #include <stdio.h>
 #include <string.h>
 
+void test(){}
+
 int main() {
     extern char data_end[], kernel_end[];
 
     memset(data_end, 0, kernel_end - data_end);
-    cons_init();
-    cprintf("hello,...%d\n",100);
-
+    //cons_init();
+    //cprintf("hello,...%d\n",100);
+	test();
     while (1)
         ;
 }
