efc5db70631ee6468619f2e5c799fc893362cfdf
fenghan <csfenghan@outlook.com>
2021-05-30 15:27:08 +0800

    删除了引用的头文件

--- boot/main.c
+++ boot/main.c
@@ -3,7 +3,25 @@
 */
 
 #include "elf.h"
-#include "x86_io.h"
+
+// 直接把头文件引进来就超过510字节了
+static inline uint8_t inb(int port) {
+        uint8_t data;
+        asm volatile("inb %w1,%0" : "=a"(data) : "d"(port));
+        return data;
+}
+
+static inline void outb(int port, uint8_t data) {
+        asm volatile("outb %0,%w1" : : "a"(data), "d"(port));
+}
+
+static inline void insl(int port, void *addr, int cnt) {
+        asm volatile("cld\n\trepne\n\tinsl"
+                     : "=D"(addr), "=c"(cnt)
+                     : "d"(port), "0"(addr), "1"(cnt)
+                     : "memory", "cc");
+}
+
 /*
     功能：从硬盘中读取数据
         sector:逻辑扇区号
@@ -23,63 +41,64 @@
 */
 //一次只能读取一个扇区的数据，即使count不为1，原因不明。仿照xv6的方式解决问题
 void read_one_sector(void *dst, uint32_t offset) {
-    while ((inb(0x1f7) & 0xc0) != 0x40)
-        ;
-    outb(0x1F2, 1);
-    outb(0x1F3, offset);
-    outb(0x1F4, offset >> 8);
-    outb(0x1F5, offset >> 16);
-    outb(0x1F6, (offset >> 24) | 0xE0);
-    outb(0x1F7, 0x20);
+        while ((inb(0x1f7) & 0xc0) != 0x40)
+                ;
+        outb(0x1F2, 1);
+        outb(0x1F3, offset);
+        outb(0x1F4, offset >> 8);
+        outb(0x1F5, offset >> 16);
+        outb(0x1F6, (offset >> 24) | 0xE0);
+        outb(0x1F7, 0x20);
 
-    while ((inb(0x1f7) & 0xc0) != 0x40)
-        ;
-    insl(0x1f0, dst, 128);
+        while ((inb(0x1f7) & 0xc0) != 0x40)
+                ;
+        insl(0x1f0, dst, 128);
 }
 
 //从kernel开始的offset地址处读取nbytes个字节到内存pa处。（就是从第二个扇区开始计算）
 void read_disk(uint32_t pa, uint32_t nbytes, uint32_t offset) {
-    uint32_t end_pa;
+        uint32_t end_pa;
 
-    end_pa = pa + nbytes;
+        end_pa = pa + nbytes;
 
-    //扇区对齐
-    pa &= ~(512 - 1);
+        //扇区对齐
+        pa &= ~(512 - 1);
 
-    //计算扇区号
-    offset = (offset / 512) + 1;
+        //计算扇区号
+        offset = (offset / 512) + 1;
 
-    while (pa < end_pa) {
-        read_one_sector((uint8_t *)pa, offset);
-        pa += 512;
-        offset++;
-    }
+        while (pa < end_pa) {
+                read_one_sector((uint8_t *)pa, offset);
+                pa += 512;
+                offset++;
+        }
 }
 
 void bootmain() {
-    Elf32_Header *elf_header;
-    Elf32_Program_Header *program_header;
+        Elf32_Header *elf_header;
+        Elf32_Program_Header *program_header;
 
-    /*这里有一个很奇怪的错误，如果吧ReadDisk中的elf_header换成0x7f00的话,
-    则读取后elf_header->e_magic值为0x7f00？？然后就无法继续执行。明明
-    elf_header的值就是0x7f00，这里有什么不一样吗？？*/
+        /*这里有一个很奇怪的错误，如果吧ReadDisk中的elf_header换成0x7f00的话,
+        则读取后elf_header->e_magic值为0x7f00？？然后就无法继续执行。明明
+        elf_header的值就是0x7f00，这里有什么不一样吗？？*/
 
-    /*把elf头加载到0x10000处*/
-    elf_header = (Elf32_Header *)0x10000;
-    read_disk((uint32_t)elf_header, 512, 0);
+        /*把elf头加载到0x10000处*/
+        elf_header = (Elf32_Header *)0x10000;
+        read_disk((uint32_t)elf_header, 512, 0);
 
-    /*判断是否是elf文件*/
-    if (elf_header->e_magic != ELF_MAGIC) {
-        return;
-    }
+        /*判断是否是elf文件*/
+        if (elf_header->e_magic != ELF_MAGIC) {
+                return;
+        }
 
-    program_header = (Elf32_Program_Header *)((void *)elf_header + elf_header->e_phoff);
-    /*将kernel的各个程序段加载至对应的位置*/
-    for (int i = 0; i < elf_header->e_phnum; i++) {
-        /*因为内核文件的加载的段都是4k对齐的，因此可以直接加载*/
-        read_disk(program_header[i].p_paddr, program_header[i].p_memsz, program_header[i].p_offset);
-    }
+        program_header = (Elf32_Program_Header *)((char *)elf_header + elf_header->e_phoff);
+        /*将kernel的各个程序段加载至对应的位置*/
+        for (int i = 0; i < elf_header->e_phnum; i++) {
+                /*因为内核文件的加载的段都是4k对齐的，因此可以直接加载*/
+                read_disk(program_header[i].p_paddr, program_header[i].p_memsz,
+                          program_header[i].p_offset);
+        }
 
-    /*终于要进去OS了！！*/
-    ((void (*)(void))(elf_header->e_entry))();
+        /*终于要进去OS了！！*/
+        ((void (*)(void))(elf_header->e_entry))();
 }
