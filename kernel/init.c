b3b2c15b318be3580ef95ce4815e02f88868e972
fenghan <csfenghan@outlook.com>
2021-05-30 18:38:25 +0800

    初始化内存

--- kernel/init.c
+++ kernel/init.c
@@ -4,14 +4,17 @@
 #include <console.h>
 #include <stdio.h>
 #include <string.h>
+#include <mm.h>
 
 int main() {
         extern char data_end[], kernel_end[];
 
         memset(data_end, 0, (size_t)kernel_end - (size_t)data_end); //清零bss段
         cons_init();                                                //输出初始化
-
-        cprintf("hello,fenghan\n");
+	mem_init();
+		
+	cprintf("hello world\n");
+	
         while (1)
                 ;
 }
